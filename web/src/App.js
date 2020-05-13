3cb1c00b711d2c3e6fe84b607bda93cdb679d5af
karoltaka <takahagassi.dev@gmail.com>
2020-05-12 21:59:46 -0300

    registering Devs

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -7,6 +8,9 @@ import './Main.css';
 
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
   const [latitude, setLatitude] = useState('');
   const [longitude, setLongitude] = useState('');
 
@@ -26,19 +30,45 @@ function App() {
       }
     )
   }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs',{
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    console.log(response.data);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
+        <form onSubmit={handleAddDev}>
           <div className="input-block">
             <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
+            <input 
+            name="github_username" 
+            id="github_username" 
+            required
+            value={github_username}
+            onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
+            <input 
+            name="techs" 
+            id="techs" 
+            required
+            value={techs}
+            onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
