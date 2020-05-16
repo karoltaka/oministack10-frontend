af0a07321edab8ca42001d61ca136d492bc699d4
karoltaka <takahagassi.dev@gmail.com>
2020-05-16 14:16:53 -0300

    search button added

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
   const [currentRegion, setCurrerntRegion ] = useState(null);
@@ -33,22 +34,39 @@ function Main({ navigation }) {
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map}>
-      <Marker coordinate={{ latitude: -7.2038863, longitude: -48.1914425 }}>
-        <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/63862417?s=460&u=a77cb40b026c35164d443fe5ef6b4c8677e0d80f&v=4'}} />
-    
-        <Callout onPress={() => {
-          //navegação
-          navigation.navigate('Profile', { github_username: 'karoltaka' });
-        }}>
-          <View style={styles.Callout}>
-            <Text style={styles.devName}>Karoline Takahagassi</Text>
-            <Text style={styles.devBio}>Apaixonada pelas melhores tecnologias de desenvolvimento Web e Mobile</Text>
-            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
-          </View>
-        </Callout>
-      </Marker>
-    </MapView>
+    <>  
+      <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -7.2038863, longitude: -48.1914425 }}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/63862417?s=460&u=a77cb40b026c35164d443fe5ef6b4c8677e0d80f&v=4'}} />
+      
+          <Callout onPress={() => {
+            //navegação
+            navigation.navigate('Profile', { github_username: 'karoltaka' });
+          }}>
+            <View style={styles.Callout}>
+              <Text style={styles.devName}>Karoline Takahagassi</Text>
+              <Text style={styles.devBio}>Apaixonada pelas melhores tecnologias de desenvolvimento Web e Mobile</Text>
+              <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+      <View style={styles.searchForm}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Buscar devs por thecs..."
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+
+        <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+          <MaterialIcons name= "my-location" size={20} color="#fff" />
+
+        </TouchableOpacity>
+
+      </View>
+    </>    
   );
 }
 
@@ -82,6 +100,42 @@ const styles = StyleSheet.create({
 
   devTechs: {
     marginTop: 5,
+  },
+
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row',
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2,
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4DFF',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   },
 })
 
