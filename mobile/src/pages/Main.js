4fc1be97d9e749321f51d6c5c4a35165a58a1335
karoltaka <takahagassi.dev@gmail.com>
2020-05-16 09:24:52 -0300

    added dev location marker

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -32,12 +32,26 @@ function Main() {
     return null;
   }
 
-  return <MapView initialRegion={currentRegion} style={styles.map} />
+  return (
+  <MapView initialRegion={currentRegion} style={styles.map} >
+    <Marker coordinate={{ latitude: -7.2038863, longitude: -48.1914425 }}/>
+      <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/63862417?s=460&u=a77cb40b026c35164d443fe5ef6b4c8677e0d80f&v=4'}} />
+  </MapView>
+  );
 }
+
 
 const styles = StyleSheet.create({
   map: {
     flex: 1
+  },
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFFF'
   },
 })
 
