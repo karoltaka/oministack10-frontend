6574f0f4ace73ae80c93f2e24057a43d759afa30
karoltaka <takahagassi.dev@gmail.com>
2020-05-19 18:39:45 -0300

    add dev filter

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -4,6 +4,10 @@ const socket = socketio('http://192.168.0.185:3333', {
   autoConnect: false,
 });
 
+function subscribeToNewDevs(subcripFunction) {
+  socket.on('new-dev', subcripFunction);
+}
+
 function connect(latitude, longitude, techs) {
   socket.io.opts.query = {
     latitude,
@@ -23,4 +27,5 @@ function disconnect() {
 export {
   connect,
   disconnect,
+  subscribeToNewDevs,
 };
